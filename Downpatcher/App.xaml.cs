08df2d6865379713f15791b2701e4a37cfe2c3fa
Matthew Dalcin <matthew_dalcin@alumni.brown.edu>
2021-01-14 01:39:52 -0500

    Fix bugs :)

--- Downpatcher/App.xaml.cs
+++ Downpatcher/App.xaml.cs
@@ -11,5 +11,12 @@ namespace Downpatcher {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        public App() : base() {
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+            MessageBox.Show("Unhandled exception occurred: \n" + e.Exception.Message + " ; " + e.Exception.StackTrace, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
